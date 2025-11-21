47eb964f81c3a6b7e89abff587def02f5bc63fa8
Dustwiper <pr574748@gmail.com>
2025-11-21 10:23:19 +0300

    Delete Source directory

--- Source/regex.cpp
+++
@@ -1,7 +0,0 @@
-#include "regex.h"
-
-namespace regExpr {
-	const std::regex strPattern { R"(^[a-z]+$)" };
-	const std::regex arrayPattern { R"(^-?\d+(?:,\s*-?\d+)*$)" };
-	const std::regex intPattern{ R"((?:-?\d+))" };
-}
--- Source/regex.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "regex"
-
-namespace regExpr {
-	extern const std::regex strPattern;
-	extern const std::regex arrayPattern;
-	extern const std::regex intPattern;
-}
-
